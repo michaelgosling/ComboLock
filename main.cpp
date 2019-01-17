9851380c9653e2f12a8411fe96b3ed7a7e683166
Michael Gosling <contact@michaelgosling.me>
2019-01-17 10:02:31 -0400

    Add code for deletion, properly retrieve user input for processing, add code for resetting, add code for substitution, add code for setting, add boolean for while loop.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 #include "LinkedList.h"
 
 std::string StringToUpper(std::string str) {
@@ -11,7 +12,7 @@ std::string StringToUpper(std::string str) {
 int main() {
     LinkedList list;
     int currPosition = 0;
-    std::string userInput;
+    bool programFinished = false;
 
     // debugging
 //    srand(time(nullptr));
@@ -22,42 +23,53 @@ int main() {
 //        std::cout << "After "<< i << ": " << ((list.Get(i)->direction == Left) ? "L" : "R") << list.Get(i)->data << std::endl;
 //    }
 
-
-    while (userInput != "Q" || userInput != "E") {
+    while (!programFinished) {
         // print list
         std::cout << list << std::endl;
         // highlight current working position
         for (int i = 0; i < NUM_OF_TURNS; i++) {
             if (i == currPosition)
-                std::cout << " ^^";
+                std::cout << " ^^ ";
             else
-                std::cout << "   ";
+                std::cout << "    ";
         }
         std::cout << std::endl << "(Q)uit without save. (E)xit with saving. (G)oto position. " << std::endl
                   << " (S)ubstitute. (D)elete. (R)eset";
         std::cout << std::endl << "Choice: ";
-        std::cin >> userInput;
-        userInput = StringToUpper(userInput);
-        auto firstLetter = userInput.c_str()[0];
+        char *userInput;
+        std::cin.getline(userInput, 256);
+        auto firstLetter = StringToUpper(userInput).c_str()[0];
         if (firstLetter == 'Q') {
             std::cout << std::endl << "Quitting without saving...";
+            programFinished = true;
         } else if (firstLetter == 'E') {
             // TODO: Save and exit code
-        } else if (firstLetter == 'G') {
-            // TODO: Goto Position code
-        } else if (firstLetter == 'S') {
-            // TODO: Substitution code
-        } else if (firstLetter == 'D') {
-            // TODO: Deletion code
-        } else if (firstLetter == 'R') {
-            // TODO: Reset code
-        } else {
-            std::cout << std::endl << "Invalid command!" << std::endl;
+        } else if (firstLetter == 'D')
+            list.Set(currPosition, 0);
+        else if (firstLetter == 'R')
+            for (int i = 0; i < NUM_OF_TURNS; i++)
+                list.Set(i, 0);
+        else {
+            // copy user input to a char array, split into tokens via space character
+            char input[strlen(userInput) + 1];
+            for (int i = 0; i < strlen(userInput); i++)
+                input[i] = userInput[i];
+            char *token;
+            strtok(input, " "); // toss first token
+            token = strtok(nullptr, " ");
+            int value = 0;
+            if (token != nullptr)
+                value = atoi(token);
+
+            // use second token differently depending on first token
+            if (firstLetter == 'G')
+                currPosition = value - 1;
+            else if (firstLetter == 'S')
+                list.Set(currPosition, value);
+            else
+                std::cout << std::endl << "Invalid command!" << std::endl;
         }
-
-
     }
-
 
 
     return 0;
